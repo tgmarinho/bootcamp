c9f0eb8f929373393c8694a805f13d38d16c72aa
Thiago Marinho <tgmarinho@gmail.com>
2019-04-23 15:08:57 -0400

    APP GoBarber - Configurando servidor

---
+++ modulo2/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+    this.middlewares()
+    this.views()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.urlencoded({ extended: false }))
+  }
+
+  views () {
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      watch: this.isDev,
+      express: this.express,
+      autoscape: true
+    })
+
+    this.express.set('view engine', 'njk')
+  }
+
+  routes () {}
+}
+
+module.exports = new App().express
