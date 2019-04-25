4e5073b53b63ad54d67eb74a1d8bda3ab2960dcd
Thiago Marinho <tgmarinho@gmail.com>
2019-04-25 16:25:15 -0400

    APP GoBarber - Agendamento de serviço

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -39,6 +39,7 @@ routes.get('/app/logout', SessionController.destroy)
 routes.get('/app/dashboard', DashboardController.index)
 
 routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
 routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
