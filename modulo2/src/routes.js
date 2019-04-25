4cd755dd7659e44a64d1ec4bf8b442044f86616b
Thiago Marinho <tgmarinho@gmail.com>
2019-04-24 21:40:04 -0400

    APP GoBarber - Listagem de prestadores

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -9,6 +9,7 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
 
 const flashMiddleware = (req, res, next) => {
   res.locals.flashSucess = req.flash('success')
@@ -30,8 +31,6 @@ routes.use('/app', authMiddleware)
 
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
