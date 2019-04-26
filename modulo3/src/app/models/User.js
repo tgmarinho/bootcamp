3a4d863c09a3365ca9eaa937dff67f9faad35c30
Thiago Marinho <tgmarinho@gmail.com>
2019-04-26 16:30:18 -0400

    APP Marketplace - Schema de usuário

---
+++ modulo3/src/app/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
