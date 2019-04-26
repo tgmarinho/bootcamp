259b794511bff0c84b334896e172ffaf01cfb08e
Thiago Marinho <tgmarinho@gmail.com>
2019-04-26 16:40:51 -0400

    APP Marketplace - Cadastro Usuário & Insomnia

--- modulo3/src/routes.js
+++ modulo3/src/routes.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
+const UserController = require('./app/controllers/UserController')
+
+routes.post('/users', UserController.store)
+
 module.exports = routes
