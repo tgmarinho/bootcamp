12f1d102bfc2081f7853453de78793e8c6d14dde
Thiago Marinho <tgmarinho@gmail.com>
2019-04-26 19:51:27 -0400

    APP Marketplace - Filtros

--- modulo3/src/app/controllers/AdController.js
+++ modulo3/src/app/controllers/AdController.js
@@ -2,15 +2,30 @@ const Ad = require('../models/Ad')
 
 class AdController {
   async index (req, res) {
-    const ads = await Ad.paginate(
-      {},
-      {
-        page: req.query.page || 1,
-        limit: 20,
-        populate: ['author'],
-        sort: '-createdAt'
+    const filters = {}
+
+    if (req.query.price_min || req.query.price_max) {
+      filters.price = {}
+      if (req.query.price_min) {
+        filters.price.$gte = req.query.price_min
       }
-    )
+      if (req.query.price_max) {
+        filters.price.$lte = req.query.price_max
+      }
+    }
+
+    if (req.query.title) {
+      filters.title = new RegExp(req.query.title, 'i')
+    }
+
+    console.log(filters)
+
+    const ads = await Ad.paginate(filters, {
+      page: req.query.page || 1,
+      limit: 20,
+      populate: ['author'],
+      sort: '-createdAt'
+    })
     return res.json(ads)
   }
 
