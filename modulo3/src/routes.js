a8f67ad8f747012fd8a0e92cd077f78a77fc97f0
Thiago Marinho <tgmarinho@gmail.com>
2019-04-26 17:49:53 -0400

    APP Marketplace - Middleware de auth

--- modulo3/src/routes.js
+++ modulo3/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const authMiddleware = require('./app/middlewares/auth')
 
 const routes = express.Router()
 
@@ -7,5 +8,9 @@ const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => {
+  res.json({ ok: true })
+})
 
 module.exports = routes
