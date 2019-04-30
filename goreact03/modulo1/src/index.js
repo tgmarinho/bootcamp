982c3293e2e62faac0fe7762c71fe2896bd1a7e7
Thiago Marinho <tgmarinho@gmail.com>
2019-04-30 18:46:18 -0400

    Go React - Módulo 1 - Criando primeiro componente

--- goreact03/modulo1/src/index.js
+++ goreact03/modulo1/src/index.js
@@ -1,3 +1,21 @@
-const soma = (a, b) => a + b;
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
 
-alert(soma(10, 10));
+class Button extends Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rockeatseat</h1>
+        <Button />
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
