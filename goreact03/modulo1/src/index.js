38664ba78331baae1f03387ba075371ea51b051f
Thiago Marinho <tgmarinho@gmail.com>
2019-04-30 21:18:21 -0400

    Go React - Módulo 1 - Métodos de ciclo de vida do React

--- goreact03/modulo1/src/index.js
+++ goreact03/modulo1/src/index.js
@@ -7,6 +7,22 @@ class App extends Component {
     counter: 0
   };
 
+  // chamar api externa ou banco de dados, é executado quando o DOM real está pronto
+  componentDidMount() {}
+
+  // utilizando quando o component deixa de exisitir na aplicação, limpar listener de eventos
+  componentWillUnmount() {}
+
+  // antes de chamar o render novamente, esse método é executado, ele retorna true ou false, se for true o render reexecuta, se false não executa o render mas o estado pode ser alterado.
+  // Usando quando quer performar um componente
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter <= 10;
+  }
+
+  // é executado depois da atualização, ele executa o render e depois executa esse método
+  // executa depois que o render executou e tenho acesso as props e estatos anterioes
+  componentDidUpdate(prevProps, prevState) {}
+
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
   };
