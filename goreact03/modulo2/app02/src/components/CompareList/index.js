c5c49359fe2acddc24953ce7bf3cf6cdf1d16ca6
Thiago Marinho <tgmarinho@gmail.com>
2019-05-03 19:31:56 -0400

    Go React - App 01 - Github Compare - buscando repositórios da API

--- goreact03/modulo2/app02/src/components/CompareList/index.js
+++ goreact03/modulo2/app02/src/components/CompareList/index.js
@@ -1,35 +1,52 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container, Repository } from "./styles";
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="Facebook"
-        />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
+    {repositories.map(repository => (
+      <Repository key={repository.id}>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
 
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string
+    })
+  ).isRequired
+};
 
 export default CompareList;
--- goreact03/modulo2/app02/src/pages/Main/index.js
+++ goreact03/modulo2/app02/src/pages/Main/index.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../assets/logo.png";
+import api from "../../services/api";
 
 import { Container, Form } from "./styles";
 
 import CompareList from "../../components/CompareList";
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">Ok</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+export default class Main extends Component {
+  state = {
+    repositoryInput: "",
+    repositories: []
+  };
 
-export default Main;
+  handleAddRepository = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+      this.setState({
+        repositories: [...this.state.repositories, response.data]
+      });
+      this.setState({ repositoryInput: "" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Ok</button>
+        </Form>
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
