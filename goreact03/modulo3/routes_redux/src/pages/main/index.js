442ca70df2bbafb56a4141624232032a3af0275a
Thiago Marinho <tgmarinho@gmail.com>
2019-05-05 15:49:01 -0400

    Go React - App 04 - Nova Aplicação - Interface da aplicação

--- goreact03/modulo3/routes_redux/src/pages/main/index.js
+++ goreact03/modulo3/routes_redux/src/pages/main/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Main = () => <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+  };
 
-export default Main;
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong>(A declarative, efficient, and flexible JavaScript
+              library for building user interfaces. https://reactjs.org )
+            </p>
+            <a href="http://https://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
