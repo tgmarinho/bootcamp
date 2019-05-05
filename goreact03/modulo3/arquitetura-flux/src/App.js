471faa9979ef764f3395d48b206c739780d58d6e
Thiago Marinho <tgmarinho@gmail.com>
2019-05-05 15:03:06 -0400

    Go React - App 03 - Arquitetura Flux com Redux - Reactotron

--- goreact03/modulo3/arquitetura-flux/src/App.js
+++ goreact03/modulo3/arquitetura-flux/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import './config/ReactotronConfig';
 import store from './store';
 
 import TodoList from './TodoList';
+
+console.tron.log('testando');
 
 const App = () => (
   <Provider store={store}>
--- goreact03/modulo3/arquitetura-flux/src/store/index.js
+++ goreact03/modulo3/arquitetura-flux/src/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+// integrando middleaware -> reactotron no redux
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...[]),
+    console.tron.createEnhancer(),
+  )
+  : applyMiddleware(...[]);
+
+const store = createStore(reducers, composer);
 
 export default store;
