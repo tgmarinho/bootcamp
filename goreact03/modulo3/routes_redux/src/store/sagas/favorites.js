d145f31b29d47126db003f38001d54326de02ebd
Thiago Marinho <tgmarinho@gmail.com>
2019-05-06 20:11:53 -0400

    Go React - App 04 - Nova Aplicação - Repositório Duplicado

--- goreact03/modulo3/routes_redux/src/store/sagas/favorites.js
+++ goreact03/modulo3/routes_redux/src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as FavoriteActions } from '../ducks/favorites';
@@ -8,15 +8,21 @@ export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
 
-    // envia para o reducer essa chamada da action
-    yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('Repositório duplicado'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+
+      // envia para o reducer essa chamada da action
+      yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (error) {
     yield put(FavoriteActions.addFavoriteFailure('Erro ao adicionar repositório'));
   }
