8d65324fb6690d14a369bda703d19bae373223e0
Thiago Marinho <tgmarinho@gmail.com>
2019-05-08 19:37:18 -0400

    Go React Native - Módulo 1 - Arquitetura e conceitos - Estilização com Flexbox

--- gonative03/modulo1/src/index.js
+++ gonative03/modulo1/src/index.js
@@ -4,59 +4,75 @@ import Todo from "./components/Todo";
 
 export default class App extends Component {
   state = {
-    counter: 0
+    usuario: "thiago",
+    todos: [{ id: 0, text: "Fazer café" }, { id: 1, text: "Estudar gonative" }]
   };
 
-  // executado quando a tela já foi renderizada
-  // melhor opção para fazer chamadas em API externa ou colocar events listeners
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({ text: "Texto Alterado" });
-    // }, 3000);
-  }
-
-  // só consegue usar as variáveis do parametro, ele não tem acesso ao state pois é estático
-  // é executado antes de montar a view e antes de cada atualização da view (chamar o render)
-  // usado apenas se a variavel do state dependa de algum valor das propriedades
-  // pouco utilizado
-  static getDerivedStateFromProps(nextProps, prevState) {
-    // return { text: nextProps.text };
-  }
-
-  // executado sempre q alterar a view e retorna um true/false para saber se vai chamar o render ou não (atualizar a view)
-  // usado para resolver alguns problemas de performance para o render não ser chamado toda hora quando atualiza a view
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextState.counter < 5;
-  }
-
-  // executa depois q atualizou e recebe o estado e props antes de ser atualizado
-  componentDidUpdate(prevProps, prevState) {}
-
-  // quando não está sendo mais renderizado a tela do component,
-  // usado para desligar event listeners e remover qualquer coisa que esteja executando em background desnecessariamente
-  componentWillMount() {}
-
-  // OBS: Não pode alterar o stado com setState nos ciclos de shouldComponentUpdate, getDerivedStateFromProps, componentDidUpdate, componentWillMount
-
-  handleAddCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: "Estudar JS" }]
+    });
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Button onPress={this.handleAddCounter} title="Add" />
-        <Text>{this.state.counter}</Text>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box}>
+          <Text style={styles.boxText}>Oi</Text>
+        </View>
       </View>
     );
   }
 }
 
+/**
+ * justifyContent acompanha a orientação do flexDirection para alinhar o conteudo
+ * se flexDirection='row' então o alinhamento é horizontal
+ * se flexDirection='column' então o alinhamento é vertical
+ */
+
+/**
+ * alignItems é ao contrário do justifyContent ( é o inverso )
+ *  se flexDirection='row' então o alinhamento é vertical
+ *  se flexDirection='column' então o alinhamento é horizontal
+ *
+ * o alignItems só funciona para uma linha de conteudo (por isso deixei só 3 box no exemplo, para caber tudo em uma linha)
+ */
+
+/**
+ * Quando tiver mais de uma linha, usar o alignContent
+ *
+ */
+
+/**
+ * No RN não tem herança de estilos
+ *
+ */
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5"
+    flex: 1, // ocupar todo espaço em tela possível
+    backgroundColor: "#333",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignContent: "space-around"
+  },
+  box: {
+    width: 80,
+    height: 80,
+    margin: 10,
+    backgroundColor: "#F00",
+    color: "#FFF", // Não tem herança,  o text de dentro do view não consegue setar branco no Text
+    // paddingHorizontal: 100
+    // marginVertical: 100
+    transform: [{ rotateZ: "20deg" }]
+  },
+  boxText: {
+    color: "#FFF"
   }
 });
