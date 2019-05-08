900ed77e0fe7a7f63f79b59c776733da8098d2d1
Thiago Marinho <tgmarinho@gmail.com>
2019-05-08 17:50:38 -0400

    Go React Native - Módulo 1 - Arquitetura e conceitos - configurando defaultProps

--- gonative03/modulo1/src/components/Todo.js
+++ gonative03/modulo1/src/components/Todo.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default class Todo extends Component {
+  static defaultProps = {
+    title: "Todo padrão"
+  };
+
   render() {
     return (
       <View>
--- gonative03/modulo1/src/index.js
+++ gonative03/modulo1/src/index.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Todo title="Fazer café" />
-        <Todo title="Fazer bolo" />
+        <Todo />
       </View>
     );
   }
