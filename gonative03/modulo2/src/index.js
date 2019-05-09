07d6bba5059204a9a633caea67d370de78fdc5d1
Thiago Marinho <tgmarinho@gmail.com>
2019-05-09 14:14:15 -0400

    Go React Native - Módulo 2 - Githuber App 2 - Estrutura inicial do projeto

---
+++ gonative03/modulo2/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+import "~/config/ReactotronConfig";
+
+console.tron.log("its working");
+
+const App = () => <View />;
+
+export default App;
