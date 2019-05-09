fd7d2d83e202860119cec59f705a59279b462884
Thiago Marinho <tgmarinho@gmail.com>
2019-05-08 21:48:12 -0400

    Go React Native - Desafio 1 - Arquitetura do Projeto e Ferramentas de apoio

---
+++ gonative03/desafio1/src/index.js
@@ -0,0 +1,35 @@
+import "./config/ReactotronConfig";
+import "./config/DevToolsConfig";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+console.tron.log("Ok... its running");
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
