dce74165b440919ef4d556adf030aa82e855c022
Thiago Marinho <tgmarinho@gmail.com>
2019-05-09 21:16:49 -0400

    Go React Native - Módulo 2 - Githuber App 2 - Consumindo lista de repos

--- gonative03/modulo2/src/pages/Repositories/index.js
+++ gonative03/modulo2/src/pages/Repositories/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
-
+import api from '~/services/api';
 import Header from '~/components/Header';
-
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositórios" />
-  </View>
-);
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
@@ -18,8 +14,33 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Repositories.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Repositories;
+  state = {
+    data: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false });
+  }
+
+  renderList = () => <Text>Lists</Text>;
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View>
+        <Header title="Repositórios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
---
+++ gonative03/modulo2/src/pages/Repositories/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+import { metrics } from '~/styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  loading: {
+    marginTop: metrics.baseMargin * 2,
+  },
+});
+
+export default styles;
