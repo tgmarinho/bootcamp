a9eeb984f8e44b965ba768fdb7da5a5aebffcbad
Thiago Marinho <tgmarinho@gmail.com>
2019-05-10 16:17:17 -0400

    Go React Native - Desafio 2 - GitIssues App 3 - Estrutura Inicial do Projeto

---
+++ gonative03/desafio2/src/styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#7a91ca',
+  secondary: '#444a5a',
+  success: '#9dca83',
+  danger: '#e37a7a',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0,0,0,0.6)',
+  whiteTransparent: 'rgba(255,255,255,0.3)',
+};
+a;
