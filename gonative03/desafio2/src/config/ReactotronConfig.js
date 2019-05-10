13f760c02d3dfe3be183d0437d29560342ef848d
Thiago Marinho <tgmarinho@gmail.com>
2019-05-10 16:23:31 -0400

    Go React Native - Desafio 2 - GitIssues App 3 - disable eslint no console and fix bug colors

--- gonative03/desafio2/src/config/ReactotronConfig.js
+++ gonative03/desafio2/src/config/ReactotronConfig.js
@@ -7,5 +7,6 @@ if (__DEV__) {
 
   tron.clear();
 
+  /* eslint-disable no-console */
   console.tron = tron;
 }
--- gonative03/desafio2/src/styles/colors.js
+++ gonative03/desafio2/src/styles/colors.js
@@ -16,4 +16,3 @@ export default {
   darkTransparent: 'rgba(0,0,0,0.6)',
   whiteTransparent: 'rgba(255,255,255,0.3)',
 };
-a;
