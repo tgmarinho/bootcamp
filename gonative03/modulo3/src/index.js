37b8c7181c39026d9da1d8a0136823addfefbc3d
Thiago Marinho <tgmarinho@gmail.com>
2019-05-12 09:08:55 -0400

    Go React Native - Módulo 3 - Masterizando o Redux - Estrutura básica do projeto

---
+++ gonative03/modulo3/src/index.js
@@ -0,0 +1,11 @@
+import "./config/DevToolsConfig";
+import "./config/ReactotronConfig";
+import React from "react";
+
+import { View } from "react-native";
+
+// import { Container } from './styles';
+
+const App = () => <View />;
+
+export default App;
