8275ea9599dc44560ee55a121241bb03c5b60ca7
Thiago Marinho <tgmarinho@gmail.com>
2019-05-13 20:16:39 -0400

    Go React Native - Módulo 3 - Masterizando o Redux - página de login

--- gonative03/modulo3/src/pages/Login/index.js
+++ gonative03/modulo3/src/pages/Login/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-
 import { View } from 'react-native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container, Input, Button, ButtonText, Error,
+} from './styles';
 
 export default class Login extends Component {
+  state = { username: '' };
+
+  handleSubmit = async () => {
+    const { username } = this.state;
+    try {
+      await api.get(`/users/${username}`);
+    } catch (error) {
+      // setar o erro
+    }
+  };
+
   render() {
-    return <View style={{ flex: 1, backgroundColor: '#000' }} />;
+    const { username } = this.state;
+    console.tron.log(username);
+
+    return (
+      <Container>
+        <Input
+          value={username}
+          onChangeText={username => this.setState({ username })}
+          autoCapitalize="none"
+          autoCorret={false}
+          placeholder="Digite seu usuário"
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
   }
 }
---
+++ gonative03/modulo3/src/pages/Login/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #7159c1;
+  align-items: stretch;
+  justify-content: center;
+  padding: 30px;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  background: #fff;
+  border-radius: 4px;
+  padding: 0 20px;
+  height: 52px;
+  font-size: 16px;
+  color: #333;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background: #5dc4b3;
+  border-radius: 4px;
+  height: 52px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const Error = styled.Text`
+  color: #ff817e;
+  margin-bottom: 20px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+`;
