0951751ea73e417ac7f754e6cb35a08e3a736251
Thiago Marinho <tgmarinho@gmail.com>
2019-05-14 15:15:15 -0400

    Go React Native - Desafio 3 - Masterizando o Redux - Estrutura inicial do projeto com lib de mapas configurado

---
+++ gonative03/desafio3/src/config/ReactotronConfig.js
@@ -0,0 +1,16 @@
+import Reactotron from "reactotron-react-native";
+import { reactotronRedux } from "reactotron-redux";
+import reactotronSaga from "reactotron-redux-saga";
+
+if (__DEV__) {
+  const tron = Reactotron.configure()
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  /* eslint-disable no-console */
+  console.tron = tron;
+}
