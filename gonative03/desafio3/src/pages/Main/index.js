067c79f9d68ef2d5edc4c8582c6b0b66efae6f29
Thiago Marinho <tgmarinho@gmail.com>
2019-05-14 15:26:10 -0400

    Go React Native - Desafio 3 - Masterizando o Redux - Estrutura inicial do projeto - fixing bugs

---
+++ gonative03/desafio3/src/pages/Main/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import MapboxGL from "@mapbox/react-native-mapbox-gl";
+
+MapboxGL.setAccessToken(
+  "pk.eyJ1IjoidGdtYXJpbmhvIiwiYSI6ImNqdm54bGtyODFtbGE0OXFsa21jYzVpaWMifQ.zPtUFMOSRpZLiLnuE7AiSQ"
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 15
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: "#7159C1",
+    transform: [{ scale: 0.8 }]
+  }
+});
+
+export default class Main extends Component {
+  renderAnnotations() {
+    return (
+      <MapboxGL.PointAnnotation
+        id="rocketseat"
+        coordinate={[-49.6446024, -27.2108001]}
+      >
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <MapboxGL.Callout title="Rocketseat House" />
+      </MapboxGL.PointAnnotation>
+    );
+  }
+
+  render() {
+    return (
+      <MapboxGL.MapView
+        centerCoordinate={[-49.6446024, -27.2108001]}
+        style={styles.container}
+        showUserLocation
+        styleURL={MapboxGL.StyleURL.Dark}
+      >
+        {this.renderAnnotations()}
+      </MapboxGL.MapView>
+    );
+  }
+}
---
+++ gonative03/desafio3/src/routes.js
@@ -0,0 +1,7 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+import Main from "~/pages/Main";
+
+const routes = createAppContainer(createSwitchNavigator({ Main }));
+
+export default routes;
