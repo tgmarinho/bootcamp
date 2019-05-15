d95a16b513d29764e8ad2bc22c6b772beb4304f3
Thiago Marinho <tgmarinho@gmail.com>
2019-05-15 19:13:00 -0400

    Go Node - Módulo 4 - Adonis - Cadastro de usuário

---
+++ gonode/modulo4/gonode/app/Controllers/Http/UserController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
+  }
+}
+
+module.exports = UserController
