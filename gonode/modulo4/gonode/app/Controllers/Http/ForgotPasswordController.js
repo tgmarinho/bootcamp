0674418823c63b781d41e01728f48984150432a6
Thiago Marinho <tgmarinho@gmail.com>
2019-05-16 10:11:41 -0400

    Go Node - Módulo 4 - Adonis - Enviando email

--- gonode/modulo4/gonode/app/Controllers/Http/ForgotPasswordController.js
+++ gonode/modulo4/gonode/app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -12,6 +13,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('no-replay@suporte.com.br', 'Adonis - Project Go Node - Mod4')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response.status(error.status).send({
         error: { message: 'Algo não deu certo, esse email existe?' }
--- gonode/modulo4/gonode/start/app.js
+++ gonode/modulo4/gonode/start/app.js
@@ -15,7 +15,9 @@ const providers = [
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
+  '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/framework/providers/ViewProvider'
 ]
 
 /*
