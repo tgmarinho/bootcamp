8758fa4cc82ff1f87c09b594ce9b1bd71cf31936
Thiago Marinho <tgmarinho@gmail.com>
2019-05-16 09:43:46 -0400

    Go Node - Módulo 4 - Adonis - Recuperação de senha

--- gonode/modulo4/gonode/database/migrations/1503250034279_user.js
+++ gonode/modulo4/gonode/database/migrations/1503250034279_user.js
@@ -5,11 +5,19 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
+    this.create('users', table => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
+      table
+        .string('username', 80)
+        .notNullable()
+        .unique()
+      table
+        .string('email', 254)
+        .notNullable()
+        .unique()
       table.string('password', 60).notNullable()
+      table.string('token')
+      table.timestamp('token_created_at')
       table.timestamps()
     })
   }
