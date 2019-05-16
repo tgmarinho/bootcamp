94a5f92ac7b861ec1f012dc6071e7a3368fda934
Thiago Marinho <tgmarinho@gmail.com>
2019-05-16 18:01:36 -0400

    Go Node - Módulo 4 - Adonis - visualizar arquivos

--- gonode/modulo4/gonode/app/Controllers/Http/FileController.js
+++ gonode/modulo4/gonode/app/Controllers/Http/FileController.js
@@ -4,6 +4,11 @@ const File = use('App/Models/File')
 const Helpers = use('Helpers')
 
 class FileController {
+  async show ({ params, response }) {
+    const file = await File.findOrFail(params.id)
+    return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+  }
+
   async store ({ request, response }) {
     try {
       if (!request.file('file')) return
--- gonode/modulo4/gonode/app/Models/File.js
+++ gonode/modulo4/gonode/app/Models/File.js
@@ -1,7 +1,16 @@
 'use strict'
 
 const Model = use('Model')
+const Env = use('Env')
 
-class File extends Model {}
+class File extends Model {
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl ({ id }) {
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
+}
 
 module.exports = File
