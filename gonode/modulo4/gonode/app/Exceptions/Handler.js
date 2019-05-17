b48fa6bf8ebb2f70123aafcf1469528682cc0f74
Thiago Marinho <tgmarinho@gmail.com>
2019-05-16 22:05:48 -0400

    Go Node - Módulo 4 - Adonis - lidando com excessões

---
+++ gonode/modulo4/gonode/app/Exceptions/Handler.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const Env = use('Env')
+const Youch = use('Youch') // formatador de erros
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle (error, { request, response }) {
+    if (error.name === 'ValidationException') {
+      return response.status(error.status).send(error.messages)
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+      const errorJSON = await youch.toJSON()
+      return response.status(error.status).send(errorJSON)
+    }
+
+    return response.status(error.status)
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report (error, { request }) {
+    console.log(error)
+  }
+}
+
+module.exports = ExceptionHandler
