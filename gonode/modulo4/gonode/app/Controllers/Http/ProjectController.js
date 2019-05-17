b5ce16e43504e0402ad2c75b5201447fa1bf44db
Thiago Marinho <tgmarinho@gmail.com>
2019-05-17 18:03:08 -0400

    Go Node - Módulo 4 - Adonis - Paginação de projetos

--- gonode/modulo4/gonode/app/Controllers/Http/ProjectController.js
+++ gonode/modulo4/gonode/app/Controllers/Http/ProjectController.js
@@ -3,10 +3,11 @@
 const Project = use('App/Models/Project')
 
 class ProjectController {
-  async index () {
+  async index ({ request }) {
+    const { page } = request.get()
     const projects = await Project.query()
       .with('user')
-      .fetch()
+      .paginate(page)
     return projects
   }
 
