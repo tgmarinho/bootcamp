32cfb9148c601002efbf95e2a30bdf9e5cd2429a
Thiago Marinho <tgmarinho@gmail.com>
2019-05-17 21:27:14 -0400

    Go Node - Módulo 4 - Utilizando transactions

--- gonode/modulo4/gonode/app/Controllers/Http/UserController.js
+++ gonode/modulo4/gonode/app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
@@ -7,9 +8,10 @@ class UserController {
     const data = request.only(['username', 'email', 'password'])
     const addresses = request.input('addresses')
 
-    const user = await User.create(data)
-
-    await user.addresses().createMany(addresses)
+    const trx = await Database.beginTransaction()
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(addresses, trx)
+    await trx.commit()
 
     return user
   }
