ed2f359650d96a15e4b194464dadef2d60510e5d
Thiago Marinho <tgmarinho@gmail.com>
2019-05-20 18:13:41 -0400

    Go Node - Desafio 4 - README resetando a senha

--- gonode/desafio4/sistac-api/app/Controllers/Http/ForgotPasswordController.js
+++ gonode/desafio4/sistac-api/app/Controllers/Http/ForgotPasswordController.js
@@ -3,6 +3,7 @@
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
+const moment = require('moment')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -32,6 +33,31 @@ class ForgotPasswordController {
     } catch (error) {
       return response.status(error.status).send({
         error: { message: 'Algo não deu certo, esse email existe?' }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'Token de recuperação está expirado' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: { message: 'Algo não deu certo ao resertar sua senha!' }
       })
     }
   }
