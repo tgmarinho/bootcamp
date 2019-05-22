cec2f0737ba0469816467cd002e56c584cdb96c2
Thiago Marinho <tgmarinho@gmail.com>
2019-05-21 20:07:34 -0400

    Go Node - Testes automatizados -  limpando a base de dados

--- gonode/modulo5/__tests__/integration/session.test.js
+++ gonode/modulo5/__tests__/integration/session.test.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
 const app = require("../../src/app");
+const truncate = require("../utils/truncate");
 const { User } = require("../../src/app/models");
 
 describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it("should be able to authenticate with valid credentials", async () => {
     const user = await User.create({
       name: "Thiago Marinho",
--- gonode/modulo5/src/config/database.js
+++ gonode/modulo5/src/config/database.js
@@ -9,6 +9,7 @@ module.exports = {
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || "postgres",
   storage: "./__tests__/database.sqlite",
+  logging: false,
   // operatorsAliases: false,
   define: {
     timestamps: true,
