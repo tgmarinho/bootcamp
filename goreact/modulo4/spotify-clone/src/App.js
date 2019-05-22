f350fbfbaf720bd55b7149a909f261f8e6c2709b
Thiago Marinho <tgmarinho@gmail.com>
2019-05-22 16:27:15 -0400

    Go React - Módulo 4 - APP Spotify - Organizando a estrutura

---
+++ goreact/modulo4/spotify-clone/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <h1>Hellooo World</h1>
+  </Fragment>
+);
+
+export default App;
---
+++ goreact/modulo4/spotify-clone/src/styles/global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    background: #181818;
+    font-family: 'Montserrat', sans-serif;
+    color: #FFF;
+  }
+`;
