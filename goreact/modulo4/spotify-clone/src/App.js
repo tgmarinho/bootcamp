a3206ce3599a36fb324e88f92160fb59f1624034
Thiago Marinho <tgmarinho@gmail.com>
2019-05-23 10:01:58 -0400

    Go React - Módulo 4 - APP Spotify - Configurando Reactotron

--- goreact/modulo4/spotify-clone/src/App.js
+++ goreact/modulo4/spotify-clone/src/App.js
@@ -1,3 +1,4 @@
+import './config/reactotron';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
--- goreact/modulo4/spotify-clone/src/pages/playlist/index.js
+++ goreact/modulo4/spotify-clone/src/pages/playlist/index.js
@@ -4,6 +4,7 @@ import { Container, Header, SongList } from './styles';
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
 
+console.tron.log('teste');
 const Playlist = () => (
   <Container>
     <Header>
