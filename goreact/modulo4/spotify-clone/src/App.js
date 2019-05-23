22d2640ec1616ff98c3c7888aebbe3f78cce4e6b
Thiago Marinho <tgmarinho@gmail.com>
2019-05-23 13:25:57 -0400

    Go React - Módulo 4 - APP Spotify - Redux e Saga

--- goreact/modulo4/spotify-clone/src/App.js
+++ goreact/modulo4/spotify-clone/src/App.js
@@ -1,6 +1,8 @@
 import './config/reactotron';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 import GlobalStyle from './styles/global';
 import SideBar from './components/SideBar';
 import Player from './components/Player';
@@ -11,19 +13,21 @@ import { Wrapper, Container, Content } from './styles/components';
 import Routes from './routes';
 
 const App = () => (
-  <BrowserRouter>
-    <GlobalStyle />
-    <Wrapper>
-      <Container>
-        <SideBar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Wrapper>
+        <Container>
+          <SideBar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
