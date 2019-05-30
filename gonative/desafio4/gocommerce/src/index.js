3bd8b2fec0f38e9082e5d525a403a6b836d81a19
Thiago Marinho <tgmarinho@gmail.com>
2019-05-30 18:19:35 -0400

    Go React Native - Desafio 4 - Estrutura inicial do projeto

---
+++ gonative/desafio4/gocommerce/src/index.js
@@ -0,0 +1,9 @@
+import '~/config/ReactotronConfig';
+import '~/config/StatusBarConfig';
+import React from 'react';
+
+import { View } from 'react-native';
+
+const App = () => <View />;
+
+export default App;
