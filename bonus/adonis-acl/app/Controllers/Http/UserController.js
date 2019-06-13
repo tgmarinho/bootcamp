220c4c8fd23a11aaf318d58a96e86ed5d5e7c99f
Thiago Marinho <tgmarinho@gmail.com>
2019-06-13 16:02:11 -0400

    Bonus - ACL Autorização com AdonisJS - Ajustando o UserController

--- bonus/adonis-acl/app/Controllers/Http/UserController.js
+++ bonus/adonis-acl/app/Controllers/Http/UserController.js
@@ -4,21 +4,53 @@ const User = use('App/Models/User')
 
 class UserController {
   async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+    const { permissions, roles, ...data } = request.only([
+      'username',
+      'email',
+      'password',
+      'permissions',
+      'roles'
+    ])
 
     const user = await User.create(data)
+
+    if (roles) {
+      await user.roles().attach(roles)
+    }
+
+    if (permissions) {
+      await user.permissions().attach(permissions)
+    }
+
+    await user.loadMany(['roles', 'permissions'])
 
     return user
   }
 
   async update ({ request, params }) {
-    const data = request.only(['username', 'email', 'password'])
+    const { permissions, roles, ...data } = request.only([
+      'username',
+      'email',
+      'password',
+      'permissions',
+      'roles'
+    ])
 
     const user = await User.findOrFail(params.id)
 
     user.merge(data)
 
     await user.save()
+
+    if (roles) {
+      await user.roles().sync(roles)
+    }
+
+    if (permissions) {
+      await user.permissions().sync(permissions)
+    }
+
+    await user.loadMany(['roles', 'permissions'])
 
     return user
   }
