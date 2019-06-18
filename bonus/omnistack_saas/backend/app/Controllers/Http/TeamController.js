41e4e1e6f4269a0410e872bdaf842aee4d11f949
Thiago Marinho <tgmarinho@gmail.com>
2019-06-18 13:14:03 -0400

    Bonus - OmniStack SaaS - backend - CRUD de times

--- bonus/omnistack_saas/backend/app/Controllers/Http/TeamController.js
+++ bonus/omnistack_saas/backend/app/Controllers/Http/TeamController.js
@@ -1,5 +1,54 @@
 'use strict'
 
-class TeamController {}
+class TeamController {
+  async index ({ auth }) {
+    const teams = await auth.user.teams().fetch()
+
+    return teams
+  }
+
+  async store ({ request, auth }) {
+    const data = request.only(['name'])
+
+    const team = await auth.user.teams().create({
+      ...data,
+      user_id: auth.user.id
+    })
+
+    return team
+  }
+
+  async show ({ params, auth }) {
+    const teams = await auth.user
+      .teams()
+      .where('teams.id', params.id)
+      .first()
+
+    return teams
+  }
+
+  async update ({ params, request, auth }) {
+    const data = request.only(['name'])
+    const team = await auth.user
+      .teams()
+      .where('teams.id', params.id)
+      .first()
+
+    team.merge(data)
+
+    await team.save()
+
+    return team
+  }
+
+  async destroy ({ params, auth }) {
+    const team = await auth.user
+      .teams()
+      .where('teams.id', params.id)
+      .first()
+
+    await team.delete()
+  }
+}
 
 module.exports = TeamController
--- bonus/omnistack_saas/backend/start/routes.js
+++ bonus/omnistack_saas/backend/start/routes.js
@@ -2,4 +2,8 @@
 
 const Route = use('Route')
 
-Route.post('/sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.group(() => {
+  Route.resource('teams', 'TeamController').apiOnly()
+}).middleware('auth')
