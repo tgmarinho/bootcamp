b547f9de34dfd859d81c7ff3c74e7c3de413f5dd
Thiago Marinho <tgmarinho@gmail.com>
2019-09-07 21:33:37 -0400

    Bootcamp - bunus - RN - Animação timing

---
+++ bonus/animacoesRN/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Animated} from 'react-native';
+
+class App extends Component {
+  state = {
+    ballY: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.ball, {top: this.state.ballY}]} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: 'red',
+  },
+});
+
+export default App;
